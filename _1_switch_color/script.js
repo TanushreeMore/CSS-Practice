de18726f4145cb18635468d7c4a2f566a19ae89c
Tanushree <tmm181092@gmail.com>
2024-03-07 16:06:56 +0530

    first commit

---
+++ _1_switch_color/script.js
@@ -0,0 +1,15 @@
+// wrapped code into DOMContentLoaded to run it after HTML fully loaded and preventing from getting null.
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.switcher-btn').onclick = () => {
+        document.querySelector('.color-switcher').classList.toggle('active');
+    };
+
+    let themeButtons = document.querySelectorAll('.theme-buttons');
+
+    themeButtons.forEach(color => {
+        color.addEventListener('click', () => {
+            let dataColor = color.getAttribute('data-color');
+            document.querySelector(':root').style.setProperty('--main-color', dataColor);
+        });
+    });
+});
